e7935da1a720b82b9f76b58ffad62ac32fedf585
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-10-02 16:17:38 +0100

    first commit for the course project. Don't really like the layout s owill be changing it

---
+++ course-project/src/app/shoppingList/app-shopping-list-edit/app-shopping-list-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-shopping-list-edit',
+  templateUrl: './app-shopping-list-edit.component.html',
+  styleUrls: ['./app-shopping-list-edit.component.scss']
+})
+export class AppShoppingListEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ course-project/src/app/shoppingList/app-shopping-list/app-shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-shopping-list',
+  templateUrl: './app-shopping-list.component.html',
+  styleUrls: ['./app-shopping-list.component.scss']
+})
+export class AppShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
