c33c746fc58d5b8b5a7fd035e9312c0504788443
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-10-02 12:39:21 +0100

    assignment 3 using directives with my solution

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayContent = false;
+  contentDBtn = "Display Content";
+  buttonClicks = [];
+  clickCounter = 0;
+  
+  toogleDisplay(event) {
+    this.displayContent = this.displayContent ? false : true;
+    
+    this.contentDBtn = (this.displayContent === true ?  "Hide Content" : "Display Content");
+
+    this.buttonClicks.push({clickNumber : this.clickCounter, timeStamp: event.timeStamp })
+    this.clickCounter++;
+  }
+  changeTextColour() {
+    return false;
+  }
 }
