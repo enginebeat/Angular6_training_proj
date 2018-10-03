5583aa1877d461bce7bd8e836ab39260b1ac310d
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-10-03 07:14:51 +0100

    started over as project structure wasn't right

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,22 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
-import { AppShoppingListComponent } from './shoppingList/app-shopping-list/app-shopping-list.component';
-import { AppShoppingListEditComponent } from './shoppingList/app-shopping-list-edit/app-shopping-list-edit.component';
-import { AppRecipeListComponent } from './recipeBook/app-recipe-list/app-recipe-list.component';
-import { AppRecipeItemComponent } from './recipeBook/app-recipe-item/app-recipe-item.component';
-import { AppRecipeDetailComponent } from './recipeBook/app-recipe-detail/app-recipe-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent,
-    AppShoppingListComponent,
-    AppShoppingListEditComponent,
-    AppRecipeListComponent,
-    AppRecipeItemComponent,
-    AppRecipeDetailComponent
   ],
   imports: [
     BrowserModule
